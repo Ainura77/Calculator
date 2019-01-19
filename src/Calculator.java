2bbb3991bcdd75757d42125d7721c5d5474ab07d
Ainura77 <43326179+Ainura77@users.noreply.github.com>
2019-01-19 17:08:23 -0500

    comment adedd

--- src/Calculator.java
+++ src/Calculator.java
@@ -12,7 +12,7 @@ public class Calculator {
         return num1 * num2;
         
         }
-    
+    // this a comment in GITHUB 1
     public static double division( double num1,double num2){
         return num1/num2;
     }
